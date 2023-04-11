25e50da4f7710f5c331c189c0cf1344fb6f1d879
breadmyburn <18ang.mss@gmail.com>
2023-04-11 18:04:47 +0800

    feat: add BaseMapper

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/mapper/UserMapper.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.sunognaisda.animelib.domain.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sunognaisda.animelib.domain.model.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserMapper {
+public interface UserMapper extends BaseMapper<User> {
 }
--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/mapper/WatchlistMapper.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/mapper/WatchlistMapper.java
@@ -1,7 +1,9 @@
 package com.sunognaisda.animelib.domain.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sunognaisda.animelib.domain.model.Watchlist;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WatchlistMapper {
+public interface WatchlistMapper extends BaseMapper<Watchlist> {
 }
