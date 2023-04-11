350b5d25ec171185d5509cae469f91cbdc8cfa54
breadmyburn <18ang.mss@gmail.com>
2023-04-11 18:05:17 +0800

    feat: add MapperScan

--- backend/animelib/src/main/java/com/sunognaisda/animelib/AnimelibApplication.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/AnimelibApplication.java
@@ -1,9 +1,11 @@
 package com.sunognaisda.animelib;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.sunognaisda.animelib.domain.mapper")
 public class AnimelibApplication {
 
 	public static void main(String[] args) {
