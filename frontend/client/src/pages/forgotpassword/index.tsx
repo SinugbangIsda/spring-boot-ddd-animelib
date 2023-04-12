f91dccfa581059a614adb88a9ff609af28dfff01
Marcu Operario <marcuoperario@gmail.com>
2023-04-12 09:22:23 +0800

    feat: add form

--- frontend/client/src/pages/forgotpassword/index.tsx
+++ frontend/client/src/pages/forgotpassword/index.tsx
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, {
+  useState
+} from 'react';
+import { Link } from 'react-router-dom';
+import { IoIosArrowRoundBack } from "react-icons/all";
+
+const INITIAL_FORM_STATE = {
+  email: ""
+}
 
 const ForgotPassword = () => {
   const [ formState, setFormState] = useState(INITIAL_FORM_STATE);
@@ -14,9 +22,65 @@ const ForgotPassword = () => {
   };
 
   return (
-    <section>
-      ForgotPassword
-    </section>
+    <section className = "min-h-screen bg-[#22272E]">
+        <div className ="flex flex-col items-center justify-center px-6 py-8 mx-auto space-y-8 md:h-screen lg:py-0">
+          <p className = "text-white font-bold text-4xl hover:cursor-pointer text-center">
+            Anime<span className = "text-[#E6613E]">Lib</span>
+          </p>
+          <div className ="w-full bg-[#2D333B] rounded-lg border-2 border-[#444C56] md:mt-0 sm:max-w-md xl:p-0">
+            <div className ="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <p className = "text-xl font-bold leading-tight tracking-tight text-center text-white">
+                Forgot Password
+              </p>
+              <form 
+                className = "space-y-4 md:space-y-6" 
+                onSubmit = {(e) => {
+                  handleSubmit(e);
+                }}
+              >
+                <div>
+                  <label 
+                    htmlFor = "email" 
+                    className = "block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Email Address
+                  </label>
+                  <input 
+                    type = "email"
+                    name = "email" 
+                    id = "email" 
+                    className = "block w-full p-2.5 bg-[#373E47] border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 rounded-lg" 
+                    placeholder = "name@company.com" 
+                    required
+                    value = { formState.email }
+                    onChange = {(e) => setFormState({
+                      ...formState,
+                      email: e.target.value
+                    })}
+                  />
+                </div>
+                <div className = "space-y-4">
+                  <button 
+                    className = "w-full text-white bg-[#E6613E] hover:bg-[#d44f2e] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-md px-5 py-2.5 select-none"
+                    type = "submit"
+                  >
+                    Submit
+                  </button>
+                  <Link 
+                    to = "/signin"
+                    className = "flex flex-row items-center space-x-1 hover:underline text-white"
+                  >
+                    <IoIosArrowRoundBack className = "text-white text-2xl hover:cursor-pointer" />
+                    <p className = "text-md">
+                      Go back to sign in
+                    </p>
+                  </Link>
+                    </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
   )
 }
 
