483f313cc4d3263f764f9fc5edb588026d62710b
breadmyburn <18ang.mss@gmail.com>
2023-04-12 16:02:40 +0800

    feat: remove the `addWatchlist` method's functionality

--- backend/animelib/src/main/java/com/sunognaisda/animelib/impl/domain/service/WatchlistServiceImpl.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/impl/domain/service/WatchlistServiceImpl.java
@@ -4,13 +4,10 @@ import com.sunognaisda.animelib.domain.model.Watchlist;
 import com.sunognaisda.animelib.domain.service.WatchlistService;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 public class WatchlistServiceImpl implements WatchlistService {
     @Override
     public Watchlist addWatchlist(Watchlist wl) {
-        wl.setId(UUID.randomUUID().toString());
         return null;
     }
 }
