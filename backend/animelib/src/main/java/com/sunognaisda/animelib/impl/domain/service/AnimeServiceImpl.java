73a0e3f24a4a359ff014b2de1c12a3c073bca783
breadmyburn <18ang.mss@gmail.com>
2023-04-12 18:26:01 +0800

    feat: update AnimeServiceImpl

--- backend/animelib/src/main/java/com/sunognaisda/animelib/impl/domain/service/AnimeServiceImpl.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/impl/domain/service/AnimeServiceImpl.java
@@ -1,8 +1,13 @@
 package com.sunognaisda.animelib.impl.domain.service;
 
+import com.sunognaisda.animelib.domain.model.Anime;
 import com.sunognaisda.animelib.domain.service.AnimeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnimeServiceImpl implements AnimeService {
+    @Override
+    public Anime addAnime(Anime anime) {
+        return null;
+    }
 }
