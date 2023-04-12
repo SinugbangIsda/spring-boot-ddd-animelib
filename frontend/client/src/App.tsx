1c075c2a8916faa02cb01f1e7205f20bd710d831
Marcu Operario <marcuoperario@gmail.com>
2023-04-12 09:44:46 +0800

    chore: remove unused modules and variables

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -3,22 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./components/privateroute";
 import { GlobalProvider } from "./context/global";
 import lazyLoad from './utils/lazyLoad';
-import { wait } from './utils/wait';
 
-const Dashboard = lazyLoad(() => wait(1000).then(() => import('./pages/dashboard')));
-const Signin = lazyLoad(() => wait(5000).then(() => import('./pages/signin')));
-const Signup = lazyLoad(() => wait(5000).then(() => import('./pages/signup')));
-const Watchlist = lazyLoad(() => wait(5000).then(() => import('./pages/watchlist')));
-const Error404 = lazyLoad(() => wait(5000).then(() => import('./pages/error404')));
-const NewForm = lazyLoad(() => wait(1000).then(() => import('./pages/newform')));
-const ForgotPassword = lazyLoad(() => wait(1000).then(() => import('./pages/forgotpassword')));
-// const Dashboard = lazyLoad(() => import('./pages/dashboard'));
-// const Signin = lazyLoad(() => import('./pages/signin'));
-// const Signup = lazyLoad(() => import('./pages/signup'));
-// const Watchlist = lazyLoad(() => import('./pages/watchlist'));
-// const Error404 = lazyLoad(() => import('./pages/error404'));
-// const NewForm = lazyLoad(() => import('./pages/newform'));
-// const ForgotPassword = lazyLoad(() => import('./pages/forgotpassword'));
+const Dashboard = lazyLoad(() => import('./pages/dashboard'));
+const Signin = lazyLoad(() => import('./pages/signin'));
+const Signup = lazyLoad(() => import('./pages/signup'));
+const Watchlist = lazyLoad(() => import('./pages/watchlist'));
+const Error404 = lazyLoad(() => import('./pages/error404'));
+const NewForm = lazyLoad(() => import('./pages/newform'));
+const ForgotPassword = lazyLoad(() => import('./pages/forgotpassword'));
 
 const App = () => {
   return (
