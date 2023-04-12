7753bbb1f21d398b82ea59a862a5e83d999d3281
Marcu Operario <marcuoperario@gmail.com>
2023-04-12 08:58:04 +0800

    feat: add states for dropdown and sidebar

--- frontend/client/src/components/applayout/index.tsx
+++ frontend/client/src/components/applayout/index.tsx
@@ -29,6 +29,10 @@ const NavItems = [
 ];
 
 const AppLayout = ({ children }: AppLayoutProps) => {
+  const [ userDropDown, setUserDropDown ] = useState(false);
+  const [ sidebarOpen, setSidebarOpen ] = useState(true);
+  const { data, dispatch } = useContext(GlobalContext);
+
   return (
     <>
       <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
