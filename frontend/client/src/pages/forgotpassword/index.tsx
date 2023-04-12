5d145c3d6fb9c974b0e640bfaca753f46996db0c
Marcu Operario <marcuoperario@gmail.com>
2023-04-12 09:17:12 +0800

    feat: add form state, handleSubmit, and handleForgotPassword

--- frontend/client/src/pages/forgotpassword/index.tsx
+++ frontend/client/src/pages/forgotpassword/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
 const ForgotPassword = () => {
+  const [ formState, setFormState] = useState(INITIAL_FORM_STATE);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleForgotPassword();
+  };
+
+  const handleForgotPassword = () => {
+    console.log("Forgot Password");
+    console.log(formState);
+  };
+
   return (
     <section>
       ForgotPassword
