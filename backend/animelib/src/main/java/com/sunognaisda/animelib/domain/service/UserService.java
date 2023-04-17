18c31e003e7f74fc6a1edbb42667b252f1a37adc
breadmyburn <18ang.mss@gmail.com>
2023-04-17 15:15:40 +0800

    refactor: rename `getUserById` method to `loginUser`, and change datatype from `Object` to `User`

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/service/UserService.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/service/UserService.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Service;
 public interface UserService {
     void registerUser(User user);
 
-    Object getUserById(long user_id);
+    User loginUser(long userId);
 
 }
