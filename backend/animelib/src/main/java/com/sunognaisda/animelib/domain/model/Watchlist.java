b9306d44b58aeb59d1e9a536ddba32cc0aa58421
breadmyburn <18ang.mss@gmail.com>
2023-04-17 15:49:21 +0800

    feat: add Composite Key function from MyBatisPlus-Plus

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/model/Watchlist.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/model/Watchlist.java
@@ -2,6 +2,7 @@ package com.sunognaisda.animelib.domain.model;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.github.jeffreyning.mybatisplus.anno.MppMultiId;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +14,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @TableName("watchlist")
 public class Watchlist {
+    @MppMultiId
     @TableField("user_id")
     private long user_id;
 
+    @MppMultiId
     @TableField("anime_id")
     private long anime_id;
 }
