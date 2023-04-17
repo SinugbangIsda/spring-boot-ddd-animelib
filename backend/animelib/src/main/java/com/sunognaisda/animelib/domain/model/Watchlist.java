70d54fc9c376e4cff3940c8e047431d16316353a
breadmyburn <18ang.mss@gmail.com>
2023-04-17 17:08:23 +0800

    refactor: change field name styles from snake_case to camelCase

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/model/Watchlist.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/model/Watchlist.java
@@ -1,6 +1,7 @@
 package com.sunognaisda.animelib.domain.model;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.github.jeffreyning.mybatisplus.anno.MppMultiId;
 import lombok.AllArgsConstructor;
@@ -15,12 +16,12 @@ import lombok.NoArgsConstructor;
 @TableName("watchlist")
 public class Watchlist {
     @MppMultiId
-    @TableField("user_id")
-    private long user_id;
+    @TableId("user_id")
+    private long userId;
 
     @MppMultiId
     @TableField("anime_id")
-    private long anime_id;
+    private long animeId;
 }
 
 
