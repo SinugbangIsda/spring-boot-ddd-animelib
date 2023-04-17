c7368ebffcd127b4dd1134bdac3cbffd76f5a8b1
breadmyburn <18ang.mss@gmail.com>
2023-04-17 16:14:19 +0800

    Changes by chatchatabc

--- backend/animelib/src/main/java/com/sunognaisda/animelib/application/rest/AnimeController.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/application/rest/AnimeController.java
@@ -7,10 +7,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/anime")
 @CrossOrigin
 public class AnimeController {
     @Autowired
     private AnimeService animeService;
+
+    @PostMapping("add")
+    public void addAnime(@RequestBody Anime anime) {
+        animeService.addAnime(anime);
+    }
+
+    @GetMapping("")
+    public List<Anime> getAllAnime() {
+        return animeService.getAllAnime();
+    }
+
+    @GetMapping("{anime_id}")
+    public Anime getAnimeById(@PathVariable("anime_id") long animeId) {
+        return animeService.getAnimeById(animeId);
+    }
+
+    @PutMapping("{anime_id}")
+    public void updateAnimeById(@PathVariable("anime_id") long animeId, @RequestBody Anime anime) {
+        anime.setId(animeId);
+        animeService.updateAnimeById(anime);
+    }
+
+    @DeleteMapping("{anime_id}")
+    public void deleteAnimeById(@PathVariable("anime_id") long animeId) {
+        animeService.deleteAnimeById(animeId);
+    }
 }
