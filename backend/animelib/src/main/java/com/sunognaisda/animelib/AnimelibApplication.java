a35282ce061eb0dab96eeb74fd2c12c30d1d01d2
breadmyburn <18ang.mss@gmail.com>
2023-04-17 15:48:34 +0800

    feat: Enable MyBatisPlus-Plus

--- backend/animelib/src/main/java/com/sunognaisda/animelib/AnimelibApplication.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/AnimelibApplication.java
@@ -1,10 +1,12 @@
 package com.sunognaisda.animelib;
 
+import com.github.jeffreyning.mybatisplus.conf.EnableMPP;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableMPP
 @MapperScan("com.sunognaisda.animelib.domain.mapper")
 public class AnimelibApplication {
 
