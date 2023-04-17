3d17d7ac165f6c769cf7c37ec90dee7bf26cd16a
breadmyburn <18ang.mss@gmail.com>
2023-04-17 17:35:49 +0800

    feat: add `addToWatchlist`, `getWatchlistByUserId`, and `deleteAnimeFromWatchlist` methods

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/service/WatchlistService.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/service/WatchlistService.java
@@ -3,8 +3,15 @@ package com.sunognaisda.animelib.domain.service;
 import com.sunognaisda.animelib.domain.model.Watchlist;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface WatchlistService {
-    Watchlist addWatchlist(Watchlist wl);
+    void addToWatchlist(Watchlist watchlist);
+
+    List<Watchlist> getWatchlistByUserId(long userId);
+
+    void deleteAnimeFromWatchlist(Watchlist watchlist);
+
 
 }
