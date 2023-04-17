990c63b24cb12ad3e3f4b5b731c6a5fb7f022c29
breadmyburn <18ang.mss@gmail.com>
2023-04-17 17:12:22 +0800

    feat: `WatchlistMapper` extends `MppBaseMapper`

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/mapper/WatchlistMapper.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/mapper/WatchlistMapper.java
@@ -1,9 +1,10 @@
 package com.sunognaisda.animelib.domain.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.jeffreyning.mybatisplus.base.MppBaseMapper;
 import com.sunognaisda.animelib.domain.model.Watchlist;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WatchlistMapper extends BaseMapper<Watchlist> {
+public interface WatchlistMapper extends MppBaseMapper<Watchlist> {
 }
