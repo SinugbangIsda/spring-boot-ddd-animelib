63bdd92f1f79f8b07ee92d585dcdf71e67f2b131
breadmyburn <18ang.mss@gmail.com>
2023-04-17 16:13:23 +0800

    feat: add `getAnimeById`, `updateAnimeById`, and `deleteAnimeById` methods

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/service/AnimeService.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/service/AnimeService.java
@@ -3,7 +3,17 @@ package com.sunognaisda.animelib.domain.service;
 import com.sunognaisda.animelib.domain.model.Anime;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface AnimeService {
-    Anime addAnime(Anime anime);
+    void addAnime(Anime anime);
+
+    List<Anime> getAllAnime();
+
+    Anime getAnimeById(long animeId);
+
+    void updateAnimeById(Anime anime);
+
+    void deleteAnimeById(long animeId);
 }
