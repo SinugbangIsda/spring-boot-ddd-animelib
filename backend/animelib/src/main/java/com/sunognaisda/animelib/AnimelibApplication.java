051aa827c728b9b0236aa4860b8c64e4ae4dcc6e
Marcu Operario <marcuoperario@gmail.com>
2023-04-18 16:02:41 +0800

    feat: add @EnableSwagger2 annotation

--- backend/animelib/src/main/java/com/sunognaisda/animelib/AnimelibApplication.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/AnimelibApplication.java
@@ -4,10 +4,12 @@ import com.github.jeffreyning.mybatisplus.conf.EnableMPP;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableMPP
 @MapperScan("com.sunognaisda.animelib.domain.mapper")
+@EnableSwagger2
 public class AnimelibApplication {
 
 	public static void main(String[] args) {
