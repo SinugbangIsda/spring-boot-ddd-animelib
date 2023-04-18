289ab96d9c91914d084913e74b0c480909083e31
Marcu Operario <marcuoperario@gmail.com>
2023-04-18 15:09:24 +0800

    feat: add UserLoginRequest

---
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/application/dto/user/UserLoginRequest.java
@@ -0,0 +1,13 @@
+package com.sunognaisda.animelib.application.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginRequest {
+    private String emailAddress;
+    private String password;
+}
