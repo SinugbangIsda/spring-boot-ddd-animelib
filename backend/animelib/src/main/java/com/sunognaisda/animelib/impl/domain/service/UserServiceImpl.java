b8b512add86b5db743350d3e4dea9247b9dda703
Marcu Operario <marcuoperario@gmail.com>
2023-04-19 14:14:15 +0800

    refactor: remove unused module

--- backend/animelib/src/main/java/com/sunognaisda/animelib/impl/domain/service/UserServiceImpl.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/impl/domain/service/UserServiceImpl.java
@@ -7,7 +7,6 @@ import com.sunognaisda.animelib.infra.util.Sha512HashUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
