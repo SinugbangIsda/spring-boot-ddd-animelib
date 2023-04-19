99accc62625b667be25a14fed1a7f1b55634b272
Marcu Operario <marcuoperario@gmail.com>
2023-04-19 14:00:57 +0800

    feat: add AnimeCreateRequest dto

---
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/application/dto/anime/AnimeCreateRequest.java
@@ -0,0 +1,20 @@
+package com.sunognaisda.animelib.application.dto.anime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnimeCreateRequest {
+    private String title;
+    private String altTitle;
+    private String type;
+    private Integer episodes;
+    private String status;
+    private String genre;
+    private String synopsis;
+    private String imageURI;
+}
+
