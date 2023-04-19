959155e65e5b75c6f716cfbcf9d52945710bc087
Marcu Operario <marcuoperario@gmail.com>
2023-04-19 14:10:43 +0800

    refactor: update Knife4jConfiguration module directory

--- backend/animelib/src/main/java/com/sunognaisda/animelib/infra/config/api/Knife4jConfiguration.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/infra/config/api/Knife4jConfiguration.java
@@ -1,4 +1,4 @@
-package com.sunognaisda.animelib;
+package com.sunognaisda.animelib.infra.config.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
