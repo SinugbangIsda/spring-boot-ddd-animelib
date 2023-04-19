44eb2cc643b810988f8e0aeb06c60e2d41e508ba
Marcu Operario <marcuoperario@gmail.com>
2023-04-19 14:11:40 +0800

    refactor: rename "UserLoginResponse" to UserResponse

--- backend/animelib/src/main/java/com/sunognaisda/animelib/application/dto/user/UserResponse.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/application/dto/user/UserResponse.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserLoginResponse {
+public class UserResponse {
     private User user;
     private ErrorContent errorContent;
 }
