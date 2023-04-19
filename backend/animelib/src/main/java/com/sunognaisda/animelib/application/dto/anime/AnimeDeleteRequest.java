c867d4f04219b7b7e545d15ab48a5c9490b7157f
Marcu Operario <marcuoperario@gmail.com>
2023-04-19 14:01:04 +0800

    feat: add AnimeDeleteRequest dto

---
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/application/dto/anime/AnimeDeleteRequest.java
@@ -0,0 +1,12 @@
+package com.sunognaisda.animelib.application.dto.anime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnimeDeleteRequest {
+    private long id;
+}
