2d1f9e7d806cfdc91150cf8fd52aa49d3f66a73d
Marcu Operario <marcuoperario@gmail.com>
2023-04-25 17:18:14 +0800

    feat: add redux and chakra providers, and update browser router implementation

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -1,70 +1,63 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PrivateRoute from "./components/privateroute";
-import { GlobalProvider } from "./context/global";
 import lazyLoad from './utils/lazyLoad';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const Dashboard = lazyLoad(() => import('./pages/dashboard'));
 const Signin = lazyLoad(() => import('./pages/signin'));
 const Signup = lazyLoad(() => import('./pages/signup'));
 const Watchlist = lazyLoad(() => import('./pages/watchlist'));
 const Error404 = lazyLoad(() => import('./pages/error404'));
-const NewForm = lazyLoad(() => import('./pages/newform'));
 const ForgotPassword = lazyLoad(() => import('./pages/forgotpassword'));
 const SelectedAnime = lazyLoad(() => import('./pages/anime'));
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: 
-      <PrivateRoute>
-        <Dashboard />
-      </PrivateRoute>,
+    path: "/",
+    element: <PrivateRoute />,
+    children: [
+      {
+        path: "",
+        element: <Dashboard />
+      },
+      {
+        path: "watchlist",
+        element: <Watchlist />
+      },
+      {
+        path: "anime/:animeId",
+        element: <SelectedAnime />
+      }
+    ]
   },
   {
-    path: '/signin',
-    element: <Signin/>,
+    path: "/*",
+    element: <Error404 />
   },
   {
-    path: '/signup',
-    element: <Signup/>,
+    path: "/signin",
+    element: <Signin />
   },
   {
-    path: '/forgotpassword',
-    element: <ForgotPassword />,
+    path: "/signup",
+    element: <Signup />
   },
   {
-    path: '/anime/:animeId',
-    element:
-      <PrivateRoute>
-        <SelectedAnime />
-      </PrivateRoute>,
-  },
-  {
-    path: '/watchlist',
-    element:
-      <PrivateRoute>
-        <Watchlist />
-      </PrivateRoute>,
-  },
-  {
-    path: '/newform',
-    element:
-      <PrivateRoute>
-        <NewForm />
-      </PrivateRoute>,
-  },
-  {
-    path: '*',
-    element: <Error404/>,
-  },
+    path: "/forgotpassword",
+    element: <ForgotPassword />
+  }
 ]);
 
 const App = () => {
   return (
-    <GlobalProvider>
-      <RouterProvider router = { router } />      
-    </GlobalProvider>
+    <Provider store = { store }>
+      <ChakraProvider>
+        <RouterProvider router = { router } />
+      </ChakraProvider>
+    </Provider>
   )
 }
 
