bd86dfa90aeea53cd0f88303253efd9c6d909371
Marcu Operario <marcuoperario@gmail.com>
2023-04-25 17:23:43 +0800

    feat: add animeService

---
+++ frontend/client/src/redux/services/animeService.ts
@@ -0,0 +1,70 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URLS } from "../../constants";
+import { getUserAndToken } from "../slices/authSlice";
+
+export const animeApi = createApi({
+    reducerPath: "animeApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: `${BASE_URLS.api}/anime`,
+        prepareHeaders: (headers) => {
+            const { token } = getUserAndToken();
+            if (token) {
+                headers.set("Authorization", `Bearer ${token}`);
+            }
+            return headers;
+        }
+    }),
+    endpoints: (builder) => ({
+        getAllAnime: builder.query({
+            query: () => ({
+                url: "/",
+                method: "GET",                                          
+                mode: "cors",
+            })
+        }),
+        getAnimeById: builder.query({
+            query: ({ animeId }) => ({
+                url: `/${animeId}`,
+                method: "GET",
+                mode: "cors",
+            })
+        }),
+        createAnime: builder.mutation({
+            query: (body) => ({
+                url: "/add",
+                method: "POST",
+                body: JSON.stringify(body),
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+        }),
+        updateAnime: builder.mutation({
+            query: ( body ) => ({
+                url: `/${body.id}`,
+                method: "PUT",
+                body: JSON.stringify(body),
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+        }),
+        deleteAnime: builder.mutation({
+            query: ({ animeId }) => ({
+                url: `/${animeId}`,
+                method: "DELETE",
+                mode: "cors",
+            })
+        })
+    })
+});
+
+export const {
+    useGetAllAnimeQuery,
+    useGetAnimeByIdQuery,
+    useCreateAnimeMutation,
+    useUpdateAnimeMutation,
+    useDeleteAnimeMutation
+} = animeApi;
