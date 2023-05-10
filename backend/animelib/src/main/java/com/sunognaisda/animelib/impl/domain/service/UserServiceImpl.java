4c48687cc0444ff5d4807dc79805fb5b8a1d8e23
breadmyburn <18ang.mss@gmail.com>
2023-05-10 11:32:55 +0800

    refactor: rename `UserMapper` to `UserRepository`

--- backend/animelib/src/main/java/com/sunognaisda/animelib/impl/domain/service/UserServiceImpl.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/impl/domain/service/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.sunognaisda.animelib.impl.domain.service;
 
-import com.sunognaisda.animelib.domain.mapper.UserMapper;
+import com.sunognaisda.animelib.domain.repository.UserRepository;
 import com.sunognaisda.animelib.domain.model.User;
 import com.sunognaisda.animelib.domain.service.UserService;
 import com.sunognaisda.animelib.infra.util.Sha512HashUtil;
@@ -12,17 +12,17 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     @Autowired
-    private UserMapper userMapper;
+    private UserRepository userRepository;
 
     @Override
     public void registerUser(User user) {
         String hashedPassword = Sha512HashUtil.encryptPassword(user.getPassword());
         user.setPassword(hashedPassword);
-        userMapper.insert(user);
+        userRepository.insert(user);
     }
 
     @Override
     public User loginUser(long userId) {
-       return userMapper.selectById(userId);
+       return userRepository.selectById(userId);
     }
 }
