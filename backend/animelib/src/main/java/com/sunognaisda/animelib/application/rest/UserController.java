1b0ac26e3d784b20c7b32b79cc111d7113c73653
breadmyburn <18ang.mss@gmail.com>
2023-05-15 15:24:41 +0800

    chore: remove methods that already exists in `AuthController` - Marcu

--- backend/animelib/src/main/java/com/sunognaisda/animelib/application/rest/UserController.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/application/rest/UserController.java
@@ -1,10 +1,7 @@
 package com.sunognaisda.animelib.application.rest;
 
-import com.sunognaisda.animelib.domain.model.User;
 import com.sunognaisda.animelib.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,20 +10,4 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     private UserService userService;
-
-    @PostMapping("register")
-    public void registerUser(@RequestBody User user){
-        userService.registerUser(user);
-    }
-
-    @GetMapping("{user_id}")
-    public User loginUser(@PathVariable("user_id") long userId) {
-        return userService.loginUser(userId);
-    }
-
-//    @PostMapping("{user_id}/validate")
-//    public User validateUser(@PathVariable("user_id") long userId, @RequestBody(required = true) User _user) {
-//        return null;
-//    }
-
 }
