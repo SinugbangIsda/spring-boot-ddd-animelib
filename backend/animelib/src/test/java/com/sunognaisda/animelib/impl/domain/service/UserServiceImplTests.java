4fb432811d83ec5cf821d0abfcf648f5374652a5
breadmyburn <18ang.mss@gmail.com>
2023-05-17 18:37:25 +0800

    test: add `testRegisterUser_UserIsRegistered()` test

---
+++ backend/animelib/src/test/java/com/sunognaisda/animelib/impl/domain/service/UserServiceImplTests.java
@@ -0,0 +1,45 @@
+package com.sunognaisda.animelib.impl.domain.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.sunognaisda.animelib.SpringBootBaseTest;
+import com.sunognaisda.animelib.domain.model.User;
+import com.sunognaisda.animelib.domain.repository.UserRepository;
+import com.sunognaisda.animelib.domain.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataSet("db/datasets/user.xml")
+public class UserServiceImplTests extends SpringBootBaseTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserServiceImpl userService;
+
+    @Test
+    void testRegisterUser_UserIsRegistered() {
+        User testUser = new User();
+        testUser.setLastName("Dalangin");
+        testUser.setFirstName("Raphael");
+        testUser.setEmailAddress("raphile14@gmail.com");
+        testUser.setImageURI("a");
+        testUser.setRole("user");
+        testUser.setPassword("Raphile14");
+
+        userService.registerUser(testUser);
+
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("email_address", testUser.getEmailAddress());
+        Optional<User> queriedUser = Optional.ofNullable(userRepository.selectOne(queryWrapper));
+
+        assertThat(queriedUser).isPresent();
+    }
+}
