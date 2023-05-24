6b418b009950724549bd5e911ed64c98c729fa2b
breadmyburn <18ang.mss@gmail.com>
2023-05-24 17:45:37 +0800

    test: add test for `testAddAnime` when an entry is added successfully

--- backend/animelib/src/test/java/com/sunognaisda/animelib/impl/domain/service/AnimeServiceImplTests.java
+++ backend/animelib/src/test/java/com/sunognaisda/animelib/impl/domain/service/AnimeServiceImplTests.java
@@ -1,34 +1,32 @@
 package com.sunognaisda.animelib.impl.domain.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.sunognaisda.animelib.SpringBootBaseTest;
 import com.sunognaisda.animelib.domain.model.Anime;
+import com.sunognaisda.animelib.domain.repository.AnimeRepository;
 import com.sunognaisda.animelib.domain.service.AnimeService;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+import java.util.Optional;
 
-@SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class AnimeServiceImplTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataSet("db/datasets/anime.xml")
+public class AnimeServiceImplTests extends SpringBootBaseTest {
+    @Autowired
+    private AnimeRepository animeRepository;
+
     @Autowired
     private AnimeService animeService;
 
     @Test
     @Order(1)
-    public void testGetAllAnime() {
-        List<Anime> testList = animeService.getAllAnime();
-        testList.forEach(System.out::println);
-    }
-
-    @Test
-    @Order(2)
-    public void testAddAnime() {
+    void testAddAnime_EntryIsAddedSuccessfully() {
+        // Set Test Values
         Anime testAnime = new Anime();
-        testAnime.setId(5);
         testAnime.setTitle("Test Title");
         testAnime.setAltTitle("Test Alt");
         testAnime.setType("Type");
@@ -38,32 +36,38 @@ public class AnimeServiceImplTests {
         testAnime.setSynopsis("Test synopsis");
         testAnime.setImageURI("test");
 
-        System.out.println(testAnime);
-
+        // Add Entry
         animeService.addAnime(testAnime);
+
+        // Check if entry exists
+        QueryWrapper<Anime> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("title", testAnime.getTitle());
+        Optional<Anime> queriedAnime = Optional.ofNullable(animeRepository.selectOne(queryWrapper));
+
+        assertThat(queriedAnime).isPresent();
     }
 
-    @Test
-    @Order(3)
-    public void testUpdateAnimeById() {
-        Anime testAnime = new Anime();
-        testAnime.setId(5);
-        testAnime.setTitle("Alt Test Title #2");
-        testAnime.setAltTitle("Test Alt");
-        testAnime.setType("Type");
-        testAnime.setEpisodes(1);
-        testAnime.setStatus("Test Status");
-        testAnime.setGenre("Test Genre");
-        testAnime.setSynopsis("Test synopsis");
+//    @Test
+//    @Order(3)
+//    public void testUpdateAnimeById() {
+//        Anime testAnime = new Anime();
+//        testAnime.setId(5);
+//        testAnime.setTitle("Alt Test Title #2");
+//        testAnime.setAltTitle("Test Alt");
+//        testAnime.setType("Type");
+//        testAnime.setEpisodes(1);
+//        testAnime.setStatus("Test Status");
+//        testAnime.setGenre("Test Genre");
+//        testAnime.setSynopsis("Test synopsis");
+//
+//        animeService.updateAnimeById(testAnime);
+//    }
 
-        animeService.updateAnimeById(testAnime);
-    }
-
-    @Test
-    public void testDeleteAnimeById() {
-        Anime testAnime = new Anime();
-        testAnime.setId(4);
-
-        animeService.deleteAnimeById(testAnime);
-    }
+//    @Test
+//    public void testDeleteAnimeById() {
+//        Anime testAnime = new Anime();
+//        testAnime.setId(4);
+//
+//        animeService.deleteAnimeById(testAnime);
+//    }
 }
