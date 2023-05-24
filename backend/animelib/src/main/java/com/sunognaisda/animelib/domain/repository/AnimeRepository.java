879554aa17f058dd257cb93136a2afc931f57ed6
breadmyburn <18ang.mss@gmail.com>
2023-05-24 17:44:44 +0800

    feat: added `getAnimeByType`, `getAnimeByStatus`, and `getAnimeByGenre` methods

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/repository/AnimeRepository.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/repository/AnimeRepository.java
@@ -2,8 +2,25 @@ package com.sunognaisda.animelib.domain.repository;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.sunognaisda.animelib.domain.model.Anime;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AnimeRepository extends BaseMapper<Anime> {
+    @Select("SELECT * " +
+            "FROM anime " +
+            "WHERE type = '${type}' AND is_deleted = 0;")
+    Optional<Anime> getAnimeByType(String type);
+
+    @Select("SELECT * " +
+            "FROM anime " +
+            "WHERE status = '${status}' AND is_deleted = 0;")
+    Optional<Anime> getAnimeByStatus(String status);
+
+    @Select("SELECT * " +
+            "FROM anime " +
+            "WHERE genre = '${genre}' AND is_deleted = 0;")
+    Optional<Anime> getAnimeByGenre(String genre);
 }
