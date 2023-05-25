d9d9a27f4f1e966980f0fe269a763c7776ff30a2
breadmyburn <18ang.mss@gmail.com>
2023-05-25 14:36:23 +0800

    test: add tests for `updateAnimeById()` and `softDeleteAnimeById()` methods

--- backend/animelib/src/test/java/com/sunognaisda/animelib/impl/domain/service/AnimeServiceImplTests.java
+++ backend/animelib/src/test/java/com/sunognaisda/animelib/impl/domain/service/AnimeServiceImplTests.java
@@ -47,6 +47,39 @@ public class AnimeServiceImplTests extends SpringBootBaseTest {
         assertThat(queriedAnime).isPresent();
     }
 
+    @Test
+    @Order(2)
+    void testUpdateAnimeById_EntryIsUpdatedSuccessfully() {
+        Anime testAnime = new Anime();
+        testAnime.setId(5);
+        testAnime.setTitle("Altered Title");
+        testAnime.setAltTitle("Title Number 2");
+        testAnime.setType("OVA");
+        testAnime.setEpisodes(50);
+        testAnime.setStatus("Completed");
+        testAnime.setGenre("Action");
+        testAnime.setSynopsis("This is an update test.");
+        testAnime.setImageURI("test");
+
+        animeService.updateAnimeById(testAnime);
+
+        QueryWrapper<Anime> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id", testAnime.getId());
+        Optional<Anime> queriedAnime = Optional.ofNullable(animeRepository.selectOne(queryWrapper));
+
+        assertThat(queriedAnime).isPresent();
+    }
+
+    @Test
+    @Order(3)
+    void testSoftDeleteAnimeById() {
+        Anime testAnime = new Anime();
+        testAnime.setId(6);
+        animeService.softDeleteAnimeById(testAnime);
+
+        animeRepository.getActiveAnimeById(6);
+    }
+
 //    @Test
 //    @Order(3)
 //    public void testUpdateAnimeById() {
