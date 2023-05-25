1306d29d607a355d61402c53559bc20c9cc3b3af
breadmyburn <18ang.mss@gmail.com>
2023-05-25 19:02:40 +0800

    feat: add `selectUserEntryByMultiId` method

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/repository/WatchlistRepository.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/repository/WatchlistRepository.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WatchlistRepository extends MppBaseMapper<Watchlist> {
@@ -15,4 +16,9 @@ public interface WatchlistRepository extends MppBaseMapper<Watchlist> {
             "INNER JOIN anime ON watchlist.anime_id = anime.id " +
             "WHERE watchlist.user_id = ${id} AND watchlist.is_deleted = 0;")
     List<Anime> getWatchlistByUserId(long id);
+
+    @Select("SELECT * " +
+            "FROM watchlist " +
+            "WHERE user_id = ${userId} AND anime_id = ${animeId}")
+    Optional<Watchlist> selectUserEntryByMultiId(long userId, long animeId);
 }
