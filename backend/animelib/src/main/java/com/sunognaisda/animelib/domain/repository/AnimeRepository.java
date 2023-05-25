472adf06a1d7f0bcc7b3cd379d0516f208c690f0
breadmyburn <18ang.mss@gmail.com>
2023-05-25 14:29:38 +0800

    feat: add `getActiveAnimeById()` and `getAllActiveAnime()` methods

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/repository/AnimeRepository.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/repository/AnimeRepository.java
@@ -5,22 +5,33 @@ import com.sunognaisda.animelib.domain.model.Anime;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AnimeRepository extends BaseMapper<Anime> {
     @Select("SELECT * " +
             "FROM anime " +
+            "WHERE id = ${id} AND is_deleted = 0;")
+    Optional<Anime> getActiveAnimeById(long id);
+
+    @Select("SELECT * " +
+            "FROM anime " +
+            "WHERE is_deleted = 0;")
+    List<Anime> getAllActiveAnime();
+
+    @Select("SELECT * " +
+            "FROM anime " +
             "WHERE type = '${type}' AND is_deleted = 0;")
-    Optional<Anime> getAnimeByType(String type);
+    List<Anime> getAnimeByType(String type);
 
     @Select("SELECT * " +
             "FROM anime " +
             "WHERE status = '${status}' AND is_deleted = 0;")
-    Optional<Anime> getAnimeByStatus(String status);
+    List<Anime> getAnimeByStatus(String status);
 
     @Select("SELECT * " +
             "FROM anime " +
             "WHERE genre = '${genre}' AND is_deleted = 0;")
-    Optional<Anime> getAnimeByGenre(String genre);
+    List<Anime> getAnimeByGenre(String genre);
 }
