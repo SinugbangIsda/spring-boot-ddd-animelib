cb12ad0984398d7821fcf1a1847170550149a653
breadmyburn <18ang.mss@gmail.com>
2023-05-26 17:26:50 +0800

    docs: create javadoc for `UserRepository` methods

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/repository/UserRepository.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/repository/UserRepository.java
@@ -10,16 +10,33 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends BaseMapper<User> {
+    /**
+     * Get data of User with the provided email address.
+     *
+     * @param emailAddress the email address of the user
+     * @return all data of the user with the provided email address
+     */
     @Select("SELECT * " +
             "FROM user " +
             "WHERE email_address = '${emailAddress}' AND is_deleted = 0;")
     Optional<User> getUserByEmail(String emailAddress);
 
+    /**
+     * Get data of all Users filtered by user role.
+     *
+     * @param role the role of the user (Admin, User)
+     * @return data of all Users belonging to the chosen role
+     */
     @Select("SELECT * " +
             "FROM user " +
             "WHERE role = '${role}'")
     List<User> getUsersByRole(String role);
 
+    /**
+     * Get data of all active Users (is_deleted = 0).
+     *
+     * @return data of all active users
+     */
     @Select("SELECT * " +
             "FROM user " +
             "WHERE is_deleted = 0")
