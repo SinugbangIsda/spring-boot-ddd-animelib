cb03cb73ba794f77c4a3c663753a888e2dadb50c
breadmyburn <18ang.mss@gmail.com>
2023-05-26 17:30:37 +0800

    test: add tests for `addToWatchlist` method when entry is a duplicate, and when added  anime does not exist refactor: change names of tests to be more descriptive

--- backend/animelib/src/test/java/com/sunognaisda/animelib/impl/domain/service/WatchlistServiceImplTests.java
+++ backend/animelib/src/test/java/com/sunognaisda/animelib/impl/domain/service/WatchlistServiceImplTests.java
@@ -2,16 +2,20 @@ package com.sunognaisda.animelib.impl.domain.service;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.sunognaisda.animelib.SpringBootBaseTest;
+import com.sunognaisda.animelib.domain.model.Anime;
 import com.sunognaisda.animelib.domain.model.Watchlist;
+import com.sunognaisda.animelib.domain.repository.AnimeRepository;
 import com.sunognaisda.animelib.domain.repository.WatchlistRepository;
 import com.sunognaisda.animelib.domain.service.WatchlistService;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataSet("db/datasets/watchlist.xml")
 public class WatchlistServiceImplTests extends SpringBootBaseTest {
@@ -21,6 +25,9 @@ public class WatchlistServiceImplTests extends SpringBootBaseTest {
     @Autowired
     WatchlistService watchlistService;
 
+    @Autowired
+    AnimeRepository animeRepository;
+
     @Test
     @Order(1)
     void testAddToWatchlist_WhenAnimeExists_ShouldAddToWatchlistSuccessfully() {
@@ -29,54 +36,53 @@ public class WatchlistServiceImplTests extends SpringBootBaseTest {
         testWatchlist.setAnimeId(4);
         watchlistService.addToWatchlist(testWatchlist);
 
-        Optional<Watchlist> queriedWatchlist = watchlistRepository.selectUserEntryByMultiId(testWatchlist.getUserId(), testWatchlist.getAnimeId());
+        Optional<Watchlist> queriedWatchlist = watchlistRepository.selectActiveWatchlistEntryByMultiId(testWatchlist.getUserId(), testWatchlist.getAnimeId());
         assertThat(queriedWatchlist).isPresent();
     }
 
-//    @Test
-//    @Order(2)
-//    void testAddToWatchlist_EntryDuplication() {
-//        Watchlist testWatchlist = new Watchlist();
-//        testWatchlist.setUserId(1);
-//        testWatchlist.setAnimeId(4);
-//        watchlistService.addToWatchlist(testWatchlist);
-//
-//        Optional<Watchlist> queriedWatchlist = watchlistRepository.selectUserEntryByMultiId(testWatchlist.getUserId(), testWatchlist.getAnimeId());
-//        assertThat(queriedWatchlist).isPresent();
-//    }
+    @Test
+    @Order(2)
+    void testAddToWatchlist_WhenEntryIsADuplicate_ShouldThrowException() {
+        Watchlist testWatchlist = new Watchlist();
+        testWatchlist.setUserId(1);
+        testWatchlist.setAnimeId(3);
 
-//    @Test
-//    @Order(2)
-//    void testAddToWatchlist_WhenAnimeDoesNotExist() {
-//        Watchlist testWatchlist = new Watchlist();
-//        testWatchlist.setUserId(1);
-//        testWatchlist.setAnimeId(5);
-//
-//        // Do a query checking if anime entry is valid
-//
-//        watchlistService.addToWatchlist(testWatchlist);
-//
-//        Optional<Watchlist> queriedWatchlist = watchlistRepository.selectUserEntryByMultiId(testWatchlist.getUserId(), testWatchlist.getAnimeId());
-//        assertThat(queriedWatchlist).isEmpty();
-//    }
+        assertThrows(DuplicateKeyException.class, () -> {watchlistService.addToWatchlist(testWatchlist);}, "DuplicateKeyException was expected");
+    }
+
+    @Test
+    @Order(2)
+    void testAddToWatchlist_WhenAnimeDoesNotExist_ShouldReturnEmpty() {
+        Watchlist testWatchlist = new Watchlist();
+        testWatchlist.setUserId(1);
+        testWatchlist.setAnimeId(5);
+
+        // Do a query checking if anime entry is valid
+        Optional<Anime> queryAnime = animeRepository.getActiveAnimeById(testWatchlist.getAnimeId());
+
+        watchlistService.addToWatchlist(testWatchlist);
+
+        Optional<Watchlist> queriedWatchlist = watchlistRepository.selectActiveWatchlistEntryByMultiId(testWatchlist.getUserId(), testWatchlist.getAnimeId());
+        assertThat(queriedWatchlist).isEmpty();
+    }
 
     @Test
     @Order(3)
-    void testSoftDeleteAnimeFromWatchlist() {
+    void testSoftDeleteAnimeFromWatchlist_WhenEntryExists_ShouldUpdateEntryAsDeleted() {
         // Test values
         Watchlist testWatchlist = new Watchlist();
         testWatchlist.setUserId(1);
-        testWatchlist.setAnimeId(4);
+        testWatchlist.setAnimeId(2);
 
-//        // Check if entry exists
-//        Optional<Watchlist> checkWatchlist = watchlistRepository.selectUserEntryByMultiId(testWatchlist.getUserId(), testWatchlist.getAnimeId());
-//        assertThat(checkWatchlist).isPresent();
+        // Check if entry exists
+        Optional<Watchlist> checkWatchlist = watchlistRepository.selectActiveWatchlistEntryByMultiId(testWatchlist.getUserId(), testWatchlist.getAnimeId());
+        assertThat(checkWatchlist).isPresent();
 
-        // Soft Delete
+        // Soft Delete (is_deleted = 1)
         watchlistService.softDeleteAnimeFromWatchlist(testWatchlist);
 
         // Check if entry cannot be found
-        Optional<Watchlist> queriedWatchlist = watchlistRepository.selectUserEntryByMultiId(testWatchlist.getUserId(), testWatchlist.getAnimeId());
+        Optional<Watchlist> queriedWatchlist = watchlistRepository.selectActiveWatchlistEntryByMultiId(testWatchlist.getUserId(), testWatchlist.getAnimeId());
         assertThat(queriedWatchlist).isEmpty();
     }
 }
