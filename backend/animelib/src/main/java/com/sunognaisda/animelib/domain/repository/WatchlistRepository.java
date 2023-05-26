f3ac713e1b25f17ddbe2070f60b3766f5a614295
breadmyburn <18ang.mss@gmail.com>
2023-05-26 17:27:04 +0800

    docs: create javadoc for `WatchlistRepository` methods

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/repository/WatchlistRepository.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/repository/WatchlistRepository.java
@@ -11,14 +11,27 @@ import java.util.Optional;
 
 @Repository
 public interface WatchlistRepository extends MppBaseMapper<Watchlist> {
+    /**
+     * Get Watchlist by User ID
+     *
+     * @param id the user id
+     * @return user's list of anime
+     */
     @Select("SELECT anime.* " +
             "FROM watchlist " +
             "INNER JOIN anime ON watchlist.anime_id = anime.id " +
             "WHERE watchlist.user_id = ${id} AND watchlist.is_deleted = 0;")
     List<Anime> getWatchlistByUserId(long id);
 
+    /**
+     * Select one entry from a user's watchlist using User ID and Anime ID
+     *
+     * @param userId the user id
+     * @param animeId the anime id
+     * @return watchlist record containing both User ID and Anime ID
+     */
     @Select("SELECT * " +
             "FROM watchlist " +
-            "WHERE user_id = ${userId} AND anime_id = ${animeId}")
-    Optional<Watchlist> selectUserEntryByMultiId(long userId, long animeId);
+            "WHERE user_id = ${userId} AND anime_id = ${animeId} AND is_deleted = 0")
+    Optional<Watchlist> selectActiveWatchlistEntryByMultiId(long userId, long animeId);
 }
