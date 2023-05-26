81f3742aa8d57a6b868dbb991cb2283d1b4bd03e
breadmyburn <18ang.mss@gmail.com>
2023-05-26 17:27:29 +0800

    docs: create javadoc for `WatchlistService` methods

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/service/WatchlistService.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/service/WatchlistService.java
@@ -5,7 +5,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface WatchlistService {
+    /**
+     * Add anime to user's watchlist
+     *
+     * @param watchlist requires a User ID and an Anime ID.
+     */
     void addToWatchlist(Watchlist watchlist);
+
+    /**
+     * Soft Delete watchlist entry by chaning `is_deleted` to 1
+     *
+     * @param watchlist requires a User ID and an Anime ID
+     */
     void softDeleteAnimeFromWatchlist(Watchlist watchlist);
 
 
