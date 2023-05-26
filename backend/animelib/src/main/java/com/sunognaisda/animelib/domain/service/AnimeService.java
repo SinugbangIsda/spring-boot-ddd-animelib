594825ffab9f3f2cd2f11cd3b33168b044dc661e
breadmyburn <18ang.mss@gmail.com>
2023-05-26 17:26:34 +0800

    docs: create javadoc for `AnimeService` methods

--- backend/animelib/src/main/java/com/sunognaisda/animelib/domain/service/AnimeService.java
+++ backend/animelib/src/main/java/com/sunognaisda/animelib/domain/service/AnimeService.java
@@ -5,9 +5,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface AnimeService {
+    /**
+     * Adds the new Anime entry into the database.
+     *
+     * @param anime contains all data of an Anime
+     */
     void addAnime(Anime anime);
 
+    /**
+     * Updates the information of the Anime entry with the provided ID.
+     *
+     * @param anime contains all data of an Anime
+     */
     void updateAnimeById(Anime anime);
 
+    /**
+     * Soft deletes the data of an Anime entry by changing `is_deleted` to 1.
+     *
+     * @param anime contains all data of an Anime
+     */
     void softDeleteAnimeById(Anime anime);
 }
